76c2fac3d925182a58d91e48fbaddc4ef3cb4d59
Christian1910n <christian.garaicoa.est@tecazuay.edu.ec>
2023-01-15 20:43:11 -0500

    guardar imagen

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,20 +1,21 @@
 import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
-import { Link,useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function AddUser() {
 
-  let navigate=useNavigate();
+  let navigate = useNavigate();
+  const [showConfirm, setShowConfirm] = useState(false);
 
 
   const [user, setUser] = useState({
     usuario: "",
     contrasena: "",
-    foto: ""
+    imagenPath: "",
   })
 
-  const { usuario, contrasena, foto } = user;
+  const { usuario, contrasena, imagenPath } = user;
 
   const onInputChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
@@ -24,44 +25,58 @@ export default function AddUser() {
   function handleImage(e) {
     console.log(e.target.files)
     setImage(e.target.files[0])
-  };
 
+
+  };
 
   function handleApi() {
     var formData = new FormData();
     formData.append("foto", image);
     axios.post("http://localhost:8080/api/assets/upload", formData, {
-      
+
       headers: {
         'Content-Type': 'multipart/form-data'
       }
-      
-    })
-
-    .then(({data}) => {
-      const { key } = data;
-      console.log(key);
-      setUser({ ...user, foto: key }); 
 
     })
-    .catch(error => {
-      console.log(error);
-    })
 
-   
+      .then(({ data }) => {
+        const { key } = data;
+        console.log("key: ", key);
+        setUser({ ...user, imagenPath: key });
+
+        console.log("Img path", user.imagenPath)
+
+      })
+      .catch(error => {
+        console.log(error);
+      })
+
+
+
 
   }
 
-  const registrar= async (e)=>{
-    e.preventDefault();
-    await axios.post("http://localhost:8080/api/cursos/crearcurso",user)
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-    navigate("/")
+
+
+  const registrar = async (e) => {
+    
+
+    if (window.confirm('¿Estas seguro que quieres registrar el usuario?')) {
+      e.preventDefault();
+
+      await axios.post("http://localhost:8080/api/cursos/crearcurso", user)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+      navigate("/")
+    } else {
+      console.log(user)
+    }
+
   }
 
   return (
@@ -69,45 +84,47 @@ export default function AddUser() {
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4"> Registro de Usuario</h2>
-          <form onSubmit={(e)=>registrar(e)}>
-          <div className="mb-3">
+          <form onSubmit={(e) => registrar(e)}>
+            <div className="mb-3">
+              <br></br>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Ingrese su usuario"
+                name="usuario"
+                value={usuario}
+                onChange={(e) => onInputChange(e)}
+              >
+              </input>
+            </div>
+            <div className="mb-3">
+              <br></br>
+              <input type={"password"}
+                className="form-control"
+                placeholder="Ingrese su contraseña"
+                name="contrasena"
+                value={contrasena}
+                onChange={(e) => onInputChange(e)}
+              >
+              </input>
+            </div>
             <br></br>
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Ingrese su usuario"
-              name="usuario"
-              value={usuario}
-              onChange={(e) => onInputChange(e)}
-            >
-            </input>
-          </div>
-          <div className="mb-3">
+            <div>
+              <input type="file" name='file' onChange={handleImage}></input>
+
+            </div>
             <br></br>
-            <input type={"password"}
-              className="form-control"
-              placeholder="Ingrese su contraseña"
-              name="contrasena"
-              value={contrasena}
-              onChange={(e) => onInputChange(e)}
-            >
-            </input>
-          </div>
-          <br></br>
-          <div>
-            <input type="file" name='file' onChange={handleImage}></input>
+            <button type="submit" className="btn btn-outline-primary"  >
+              Registrar
+            </button>
 
-          </div>
-          <br></br>
-          <button type="submit" className="btn btn-outline-primary" onClick={handleApi} >
-            Registrar
-          </button>
 
-          
-          <Link
-            className="btn btn-outline-danger mx-2" to>
-            Cancelar
+            <Link
+              className="btn btn-outline-danger mx-2" to onClick={handleApi}  >
+              Guardar Imagen
             </Link>
+
+
           </form>
         </div>
 
--- src/ventanas/home.js
+++ src/ventanas/home.js
@@ -2,17 +2,42 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function Home() {
-    
-    const [users,setUsers]=useState([])
-    
-    useEffect(()=>{
-        loadUsers();
-    },[])
 
-    const loadUsers=async()=>{       const result = await axios.get("http://localhost:8080/api/cursos/getcurso");
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        loadUsers();
+    }, [])
+
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:8080/api/cursos/getcurso");
         console.log("F")
         setUsers(result.data);
     };
+
+
+    axios.get('http://localhost:8080/api/cursos/validarLogin', {
+        params: {
+            usuario: "christi",
+            contrasena: "1234"
+        }
+    })
+        .then(response => {
+            console.log(response.data);
+            if(response.data== true){
+                console.log("Inicio de sesion exitoso")
+            }else{
+                console.log("Inicio de sesion fallido")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+
+
+
+
 
     return (
         <div className='container'>
@@ -28,15 +53,15 @@ export default function Home() {
                     </thead>
                     <tbody>
                         {
-                            users.map((user,index)=>(
+                            users.map((user, index) => (
                                 <tr>
-                                <th scope="row" key={index}>{index+1}</th>
-                                <td>{user.usuario}</td>
-                                <td>{user.contrasena}</td>
-                                <td>{user.imagenUrl}</td>
-                            </tr>
+                                    <th scope="row" key={index}>{index + 1}</th>
+                                    <td>{user.usuario}</td>
+                                    <td>{user.contrasena}</td>
+                                    <img src={user.imagenUrl} alt="Mi imagen" width="150px" />
+                                </tr>
                             ))
-                        }  
+                        }
                     </tbody>
                 </table>
             </div>
